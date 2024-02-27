0fcc29dac850dc0e6efd22d7c8a63a75a444224b
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-02-27 14:46:36 +0700

    Add user table migration and entity

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::sync::Arc;
 
-use parking_lot::RwLock;
+use anyhow::Error as anyError;
+use migration::MigratorTrait;
+use parking_lot::lock_api::RwLock;
+use parking_lot::RawRwLock;
 use sea_orm::ConnectOptions;
 use sea_orm::Database;
+use sea_orm::DatabaseConnection;
 use shuttle_runtime::Error;
 use warp::Filter;
 use warp::Reply;
@@ -15,12 +19,15 @@ async fn warp(
     conn_str: String,
 ) -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
     let connect_opt = ConnectOptions::new(conn_str);
-    let db = Arc::new(RwLock::new(
-        Database::connect(connect_opt)
-            .await
-            .map_err(|e| Error::Database(e.to_string()))?,
-    ));
-    let db_filter = warp::any().map(move || Arc::clone(&db));
+    let db = Database::connect(connect_opt)
+        .await
+        .map_err(|e| Error::Database(e.to_string()))?;
+    migration::Migrator::up(&db, None)
+        .await
+        .map_err(|e| Error::Custom(anyError::msg(format!("Error running migrations: {e}"))))?;
+
+    let db_clone = Arc::new(RwLock::<RawRwLock, DatabaseConnection>::new(db));
+    let db_filter = warp::any().map(move || Arc::clone(&db_clone));
 
     let route = warp::any()
         .and(db_filter)
